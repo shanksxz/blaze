2c8904baac47ddf174013f53e2724da9ec01963d
somya bhatt <somyacu1@gmail.com>
2024-12-04 23:42:50 +0530

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,29 +2,40 @@ import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { env } from './env';
+import { cookies } from 'next/headers';
 
 const publicPaths = ['/', '/login'];
+const isProduction = process.env.NODE_ENV === 'production';
 
 export async function middleware(request: NextRequest) {
-	const token = await getToken({
-		req: request,
-		secret: env.AUTH_SECRET,
-		raw: true,
-	});
 
-	const isPublicPath = publicPaths.includes(request.nextUrl.pathname);
+  let token = "";
+  if(!isProduction) {
+    token = await getToken({
+      req: request,
+      secret: env.AUTH_SECRET,
+      raw: true,
+    });
+  } else {
+    const cookie = (await cookies()).get('__Secure-authjs.session-token');
+    token = cookie?.value || '';
+  }
+
+  const isPublicPath = publicPaths.includes(request.nextUrl.pathname);
 
 	if (isPublicPath) {
 		return NextResponse.next();
 	}
 
-	if (!token) {
-		return NextResponse.redirect(new URL('/login', request.url));
-	}
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 
-	return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
-	matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
 };
