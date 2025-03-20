cc92917779ddb524e7ce9dbb59259f61f32de964
somya bhatt <somyacu1@gmail.com>
2025-03-21 03:52:07 +0530

    clean-ups^^

--- apps/web/src/app/(main)/layout.tsx
+++ apps/web/src/app/(main)/layout.tsx
@@ -1,16 +1,16 @@
 import AppHeader from "@/components/layout/app-header";
 import { AppSidebar } from "@/components/layout/app-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<SidebarProvider>
 			<div className="flex min-h-screen w-full">
 				<AppSidebar />
-				<div className="flex-1 flex flex-col bg-background">
+				<SidebarInset>
 					<AppHeader />
 					<main className="flex-1 container mx-auto px-6 py-4">{children}</main>
-				</div>
+				</SidebarInset>
 			</div>
 		</SidebarProvider>
 	);
